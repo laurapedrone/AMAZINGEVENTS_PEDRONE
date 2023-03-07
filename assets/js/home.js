93b48cf382044ce77f1be03b1805b79017ecc6a8
Laura Pedrone <lau.pedrone@gmail.com>
2023-03-07 19:54:15 -0300

    Modificación en el diseño

--- assets/js/home.js
+++ assets/js/home.js
@@ -7,14 +7,14 @@ mostrarCards(events, contenedorCardsEvents);
 function mostrarCards(arrayCards, contenedor) {
   let cards = "";
   for (datas of arrayCards) {
-    cards += `<div class="news-block-horizontal-one mb-20 d-flex justify-content-center p-3">
-    <div class="news-block-horizontal-img col-lg-6 col-sm-6">
+    cards += `<div class="news-block-horizontal-one mb-20 d-flex justify-content-center p-3 ">
+    <div class="news-block-horizontal-img col-lg-6 col-sm-12 col-md-6">
       <a href="#">
         <img class="imageCard" src="${datas.image}" alt="Cinema">
       </a>
     </div>
-    <div class="col-lg-6 col-sm-6 news-block-horizontal-content d-flex flex-wrap gap-2">
-      <div class="meta d-flex gap-5">
+    <div class="col-lg-6 col-sm-12 col-md-6 news-block-horizontal-content d-flex flex-wrap gap-2 flex-column">
+      <div class="meta">
         <a href="./details.html">${datas.category}</a>
         <p>${datas.date}</p>
       </div>
@@ -31,7 +31,7 @@ function mostrarCards(arrayCards, contenedor) {
         <p>Capacity: ${datas.capacity}</p>
         <p>Assistance: ${datas.assistance}</p>
       </div>
-      <div class="d-flex gap-5">
+      <div class="d-flex justify-content-between">
         <h5>Price $${datas.price}</h5>
         <a href="./details.html" class="btn-style-1">Read More <i class="fa fa-chevron-right"></i></a>
       </div>
